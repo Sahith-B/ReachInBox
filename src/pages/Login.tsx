ca840f83adafe5c2112a2efe10871a4ee87140d6
Sahith-B <124864152+Sahith-B@users.noreply.github.com>
2024-08-09 15:21:51 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,7 +14,7 @@ function Login() {
   }
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-in-box-wheat.vercel.app/login";
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-in-box-wheat.vercel.app/";
   };
 
   return (
